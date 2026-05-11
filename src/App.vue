03a3041cc85cabf11c3fe3a5f0fc572e29fedd70
wangzm <1466418631@qq.com>
2026-05-11 18:40:19 +0800

    chore: lint自动修复 + 删除CLAUDE.md

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -25,13 +25,12 @@
   }
 }
 
-
 .el-input.is-disabled .el-input__inner {
   color: #0a0a0a !important;
 }
 
 .el-radio__input.is-checked + .el-radio__label {
-  color: #409EFF !important;
+  color: #409eff !important;
 }
 
 .problem-section {
